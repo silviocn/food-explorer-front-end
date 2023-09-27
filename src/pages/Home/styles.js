22ad13d3503cbd8f16dbbeaaa3d7e7d8e286c14b
silvio <silviocostaadm@gmail.com>
2023-09-27 14:02:38 -0300

    fixing bugs

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -158,7 +158,6 @@ export const Main = styled.main`
       width: 200px;
       height: 150px;
       margin-top: 0px;
-      overflow-y: auto;
   }
       margin-top: -50px;
       margin-left: -40px;

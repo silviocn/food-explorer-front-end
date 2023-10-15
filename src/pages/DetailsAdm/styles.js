4408895abed4c95051cccd8a87f37b31ac2d9b7a
silvio <silviocostaadm@gmail.com>
2023-10-15 12:19:39 -0300

    fixing bugs

--- src/pages/DetailsAdm/styles.js
+++ src/pages/DetailsAdm/styles.js
@@ -121,9 +121,10 @@ export const Main = styled.main`
     height: 350px;
   }
   svg {
+    align-items: end; // alterado 15/10
     color: ${({ theme }) => theme.COLORS.WHITE_200};
-    width: 18px;
-    height: 18px;
+    width: 24px; // alterado
+    height: 22px; // alterado
   }
   .button {
         background: none;

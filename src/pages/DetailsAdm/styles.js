12e0f33ea1feb9aea143920dc06556ffd4e7aec7
silvio <silviocostaadm@gmail.com>
2023-10-19 09:10:37 -0300

    fixing bugs

--- src/pages/DetailsAdm/styles.js
+++ src/pages/DetailsAdm/styles.js
@@ -149,3 +149,117 @@ export const Main = styled.main`
           transform: scale(1.2)
       }
 `;
+
+export const Header = styled.header`
+  grid-area: header;
+  height: 104px;
+  width: 100%;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 125px;
+  white-space: nowrap;
+  animation: topdown 1s;
+  @media (max-width: 820px) { // Ipad Air
+    height: 120px;
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    align-items: start;
+    justify-content: center;
+   }
+  @keyframes topdown {
+      0% {
+        opacity: 0;
+        transform: translateY(-20px)
+      }
+      100% {
+        opacity: 1;
+        transform: translateY(0)
+      }
+    }
+  a{
+    color: ${({ theme }) => theme.COLORS.WHITE_200};
+  }
+  .gap {
+    display: flex;
+    width: 740px;
+    align-items: center;
+    gap: 16px;
+    @media (max-width: 820px) { // Ipad Air
+      flex-direction: column;
+      width: 30%;
+      gap: 10px;
+    }
+  }
+  .logo {
+    display: flex;
+    align-items: center;
+    gap: 11px;
+    padding-right: 37px;
+    svg {
+      color: ${({ theme }) => theme.COLORS.BLUE_100};
+      width: 29.2px;
+      height: 29.2px;
+    }
+    span {
+      font-family: 'Roboto';
+      color: ${({ theme }) => theme.COLORS.WHITE_200};
+      font-weight: bold;
+      font-size: 25px;
+    }
+    h2 {
+      font-family: 'Roboto';
+      color: ${({ theme }) => theme.COLORS.BLUE_200};
+      font-weight: small;
+      font-size: 10px;
+      position: absolute;
+      margin-left: 160px;
+      margin-top: 45px;
+    }
+    @media (max-width: 820px) { // Ipad Air
+      gap: 5px;
+      padding: 1px;
+      svg {
+        width: 15px;
+        height: 15px;
+      }
+      span {
+        font-size: 15px;
+      }
+      h2 {
+        position: absolute;
+        margin-left: 120px;
+        margin-top: 5px;
+      }
+    }
+  }
+  /*.buttons {
+    display: flex;
+    gap: 20px;
+  }*/
+`;
+
+export const Input = styled.div`
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_500};
+  width: 100%;
+  height: 48px;
+  display: flex;
+  align-items: center;
+  border-radius: 5px;
+  padding: 16px;
+  input {
+    color: ${({ theme }) => theme.COLORS.WHITE_200};
+    padding-left: 16px;
+    width: 100%;
+    background: transparent;
+    border: none;
+  }
+  svg {
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+    width: 19.5px;
+    height: 19.5px;
+  }
+`;

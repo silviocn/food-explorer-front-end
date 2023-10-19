982ca762dafccd45e7ed7b41d508e1086fffe7bb
silvio <silviocostaadm@gmail.com>
2023-10-19 18:30:36 -0300

    fixing bugs

--- src/components/Favorite/styles.js
+++ src/components/Favorite/styles.js
@@ -4,11 +4,12 @@ export const Container = styled.div`
   display: flex;
   margin-top: 16px;
   margin-right: 18px;
+  margin-bottom: -25px;
   place-self: end;
-  
+  scale: 1.5;
+
   svg {
     color: ${({ theme }) => theme.COLORS.GRAY_400};
-    
     transition: transform 0.5s;
   }
   svg:hover {
